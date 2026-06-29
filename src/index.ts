8be48b317e8fe47178b90f1ca8d4ae8653352b72
Clint <cahenrichs@gmail.com>
2026-06-29 12:12:09 -0500

    added the main func to look for args

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,13 @@
+function main() {
+if (process.argv.length === 2) {
+    process.exit(1)
+} else  if (process.argv.length > 3) {
+    process.exit(1)
+}
+const baseURL = process.argv[2];
+console.log(`Crawler is starting at ${baseURL}`)
+process.exit(0)
 
-console.log("Hello, World!")
+}
+
+main();
